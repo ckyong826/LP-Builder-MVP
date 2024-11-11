7ce6d5565ff0ebd3030d479586fda1f08fa6c3d4
Ck Yong <118044421+ckyong826@users.noreply.github.com>
2024-11-11 23:40:32 +0800

    Feat : Library Demo Feature

--- frontend/src/app/library/page.tsx
+++ frontend/src/app/library/page.tsx
@@ -6,19 +6,16 @@
 // GrapeJS
 import grapesjs, { Editor } from "grapesjs";
 import "grapesjs/dist/css/grapes.min.css";
-import GjsEditor, { Canvas } from "@grapesjs/react";
+import GjsEditor from "@grapesjs/react";
 import gPreset from "grapesjs-preset-webpage";
 import gBlocks from "grapesjs-blocks-basic";
+import gCkEditor from "grapesjs-plugin-ckeditor";
 import gFlexbox from "grapesjs-blocks-flexbox";
-import gCountDown from "grapesjs-component-countdown";
 import gCustomCode from "grapesjs-custom-code";
 import gNavbar from "grapesjs-navbar";
-import gCkEditor from "grapesjs-plugin-ckeditor";
 import gExport from "grapesjs-plugin-export";
-import gForms from "grapesjs-plugin-forms";
 import gStyleBg from "grapesjs-style-bg";
-import gStyleGradient from "grapesjs-style-gradient";
-import gTooltip from "grapesjs-tooltip";
+import gToolTip from "grapesjs-tooltip";
 
 export default function CustomEditor() {
   const onEditor = (editor: Editor) => {
@@ -38,20 +35,14 @@ export default function CustomEditor() {
       plugins={[
         gBlocks,
         gFlexbox,
-        gCountDown,
+        gPreset,
         gCustomCode,
         gNavbar,
-        gCkEditor,
         gExport,
-        gForms,
+
         gStyleBg,
-        gStyleGradient,
-        gTooltip,
+        gToolTip,
       ]}
-    >
-      <div>
-        <Canvas />
-      </div>
-    </GjsEditor>
+    ></GjsEditor>
   );
 }
